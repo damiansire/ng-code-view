126abbb22187596e1c4700d1a6a600f1e9ddd519
Damian Sire <damiansire@gmail.com>
2025-02-14 18:50:04 +0700

    wip: add tailwind css

--- projects/design-system/src/public-api.ts
+++ projects/design-system/src/public-api.ts
@@ -5,5 +5,6 @@
 export * from './lib/components/button/button.component';
 export * from './lib/components/header/header.component';
 export * from './lib/components/page/page.component';
+export * from './lib/components/code-view/code-view.component';
 export * from './lib/design-system.component';
 export * from './lib/design-system.service';
